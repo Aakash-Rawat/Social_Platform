fbb6574fd45c6c6d97c0068cd941a3a8b8dabc97
Aakash-Rawat <rawataakash.ar@gmail.com>
2025-04-25 03:29:25 +0530

    cloudinary,multer,follow/unfollow,register,apiTesting

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,49 +1,50 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
+import { Post } from "../models/post.model.js";
+
 
 // Registering User
 export const register = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      return res.status(401).json({
-        message: "Something is missing, please check",
-        success: false,
+      const { username, email, password } = req.body;
+      if (!username || !email || !password) {
+          return res.status(401).json({
+              message: "Something is missing, please check!",
+              success: false,
+          });
+      }
+      const user = await User.findOne({ email });
+      if (user) {
+          return res.status(401).json({
+              message: "Try different email",
+              success: false,
+          });
+      };
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await User.create({
+          username,
+          email,
+          password: hashedPassword
       });
-    }
-
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(401).json({
-        message: "User already exist",
-        success: false,
+      return res.status(201).json({
+          message: "Account created successfully.",
+          success: true,
       });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    return res.status(201).json({
-      message: "User registered Successfully",
-      success: true,
-    });
   } catch (error) {
-    console.log(error);
+    return res.status(500).json({ message: "Server error", error: error.message });
   }
-};
+  }
+
 
 
 // Login User
 export const login = async (req,res) =>{
     try {
-        const { username, email, password } = req.body;
-    if (!username || !email || !password) {
+        const {  email, password } = req.body;
+    if (!email || !password) {
       return res.status(401).json({
         message: "Something is missing, please check",
         success: false,
@@ -58,13 +59,13 @@ export const login = async (req,res) =>{
         })
     }
 
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
-      if(!isPasswordMatch){
-        return res.status(401).json({
-            message: "Incorrect credentials",
-            success: false
-        })
-      }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            });
+        };
 
       user = {
         _id: user._id,
@@ -78,7 +79,7 @@ export const login = async (req,res) =>{
 
       }
 
-      const token = await jwt.sign({userId: user._id},process.env.SECRET_KEY,{expiresIn: '1d'})
+      const token =  jwt.sign({userId: user._id},process.env.SECRET_KEY,{expiresIn: '1d'})
       return res.cookie('token',token,{httpOnly:true, sameSite:'strict', maxAge: 1*24*60*60*1000}).json({
         message: `Welcome Back ${user.username}`,
         success: true,
@@ -92,7 +93,7 @@ export const login = async (req,res) =>{
 }
 
 //Log Out
-export const logout = async (req,res)=>{
+export const logout = async (_,res)=>{
        try {
              return res.cookie('token',"", {maxAge:0}).json({
                 message: "Logged Out Successfully",
@@ -111,7 +112,7 @@ export const getProfile = async (req,res)=>{
        try {
         
              const userId = req.params.id;
-             let user = await User.findById(userId);
+             let user = await User.findById(userId).select('-password');
              return res.status(200).json({
                 user,
                 success: true
@@ -127,16 +128,91 @@ export const editProfile = async (req,res)=>{
          try {
             
             const userId = req.id;
+            const {bio,gender} = req.body;
+            const profilePicture = req.file;
+            let cloudResponse;
+            if(profilePicture){
+              const fileUri = getDataUri(profilePicture);
+             cloudResponse =  await cloudinary.uploader.upload(fileUri);
+            }
+           
+            const user = await User.findById(userId).select("-password");
+              if(!user){
+                return res.status(404).json({
+                  message: "User not found",
+                  success: false
+                })
+              }
             
+              if(bio){
+                user.bio = bio;
+              }
 
+              if(gender){
+                user.gender = gender;
+              }
 
+              if(profilePicture){
+                user.profilePicture = cloudResponse.secure_url;
+              }
 
+              await user.save();
+             return res.status(200).json({
+              message: "Profile Updated",
+              success : true,
+              user
+             })
 
             
          } catch (error) {
-            console.log(error);
+            console.log(error)
             
          }
+}
 
+// Follow & Unfollow
+export const followOrUnfollow = async (req,res)=>{
+  try {
+     
+       const followBy = req.id;
+       const followTo = req.params.id;
+       if(followBy===followTo){
+          return res.status(400).json({
+            message: 'You cannot follow/unfollow yourself',
+            success: false
+          })
+       } 
+        
+       const user = await User.findById(followBy);
+       const targetUser = await User.findById(followTo);
+
+       if(!user || !targetUser ){
+          return res.status(400).json({
+            message: 'User not',
+            success: false
+          })
+       }
+      
+       const isFollowing = user.following.includes(followTo);
+
+       if(isFollowing){
+        await Promise.all([
+          User.updateOne({_id: followBy},{$pull:{following: followTo}}),
+          User.updateOne({_id: followTo},{$pull:{followers: followBy}})
+         ])
+         return res.status(200).json({message: 'Unfollow successfully', sccess: true});
+       }
+       else{
+             await Promise.all([
+              User.updateOne({_id: followBy},{$push:{following: followTo}}),
+              User.updateOne({_id: followTo},{$push:{followers: followBy}})
+             ])
+             return res.status(200).json({message: 'Followed successfully', sccess: true});
+       } 
+
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
 
--- backend/index.js
+++ backend/index.js
@@ -3,33 +3,29 @@ import dotenv from"dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./utils/db.js";
-dotenv.config({});
+import userRoute from './routes/user.routes.js'
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+
+dotenv.config();
 
 
-app.get("/",(req,res)=>{
-    return res.status(200).json({
-        message: "I'm coming from backend",
-        success: true
-    })
-})
+const PORT = process.env.PORT || 3000;
 
 
 // middlewares
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(urlencoded({extended:true}));
 const corsOptions = {
-    origin: 'https://localhost:5173',
+    origin: '*',
     credentials: true
 };
 app.use(cors(corsOptions));
 
 
-
+app.use("/api/v1/user", userRoute)
 
 
 app.listen(PORT,()=>{
