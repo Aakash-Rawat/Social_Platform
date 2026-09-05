ede40a39f52f5e5d38da3e03cc0333646c395725
Aakash Rawat <rawataakash.ar@gmail.com>
2026-09-06 04:02:48 +0530

    Remove specific CORS options and enable all origins

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({extended:true}));
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use("/api/v1/user", userRoute)
